31a0832e947e91640a01445629142ff13f44d7c0
pragati0209 <pragatiaggimath09@gmail.com>
2026-01-19 12:15:32 +0530

    Update test_simple_interest.py

--- test_simple_interest.py
+++ test_simple_interest.py
@@ -1,6 +1,10 @@
-def simple_interest(principal,rate,time);
-     principal=("principal",p);
-     Rate=("rate",r);
-     Time=("time",t);
-     simple_interest=p*t*r/100
-assert_info="expected output"
+def simple_interest(principal, rate, time):
+    si = (principal * rate * time) / 100
+    return si
+p = 500000
+r = 2
+t = 2
+result = simple_interest(p, r, t)
+print("Simple Interest", result)
+
+assert result == 20000.0
