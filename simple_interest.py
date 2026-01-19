77375020aa2e72849e9a89384b3b2d0f2658d4ef
pragati0209 <pragatiaggimath09@gmail.com>
2026-01-19 11:23:24 +0530

    save

---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+import sys
+simple_interest_info("principal,rate,time")
+       input=("Principal",p);
+       input=("Rate",r);
+       input=("Time",t);
+
+_ _"int"_ _=_ _"main"_ 
+       print("Principal",500000)
+       print("Rate",2000)
+       print("Time",2 year)
+assert_info="expected output"(principal,rate,time);
---
+++ test_simple_interest.py
@@ -0,0 +1,6 @@
+def simple_interest(principal,rate,time);
+     principal=("principal",p);
+     Rate=("rate",r);
+     Time=("time",t);
+     simple_interest=p*t*r/100
+assert_info="expected output"
